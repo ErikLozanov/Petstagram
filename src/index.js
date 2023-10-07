ac012309fb6e2c036520ec7ac3a572c0da53bf4d
Erik Lozanov <wolfierecordss@gmail.com>
2023-10-07 12:49:51 +0300

    Add static + body parser middlewares

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: false}));
+
 app.get('/', (req, res) => {
     res.send('First Action');
 });
